0c6eb676f8d0d288814f04758159475a479ef0f8
yuryev <vikor.yuryev@gmail.com>
2021-12-06 20:59:56 +0400

    Revert "Add Actor and Director Add maven"
    This reverts commit 350bb5c7

--- src/Genre/Genre.java
+++ src/Genre/Genre.java
@@ -1,18 +1,12 @@
 package Genre;
 
-import java.util.UUID;
-
 public class Genre {
 
-    private String id;
-    private String tittle;
+    String tittle;
 
     Genre(String newGener){
-        this.id = UUID.randomUUID().toString();
         tittle = newGener;
     }
-
-    public String getId(){return this.id;}
 
     public String getTittle() {
         return tittle;
