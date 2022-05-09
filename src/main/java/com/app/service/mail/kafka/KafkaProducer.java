d504d5562960444a563bca8b06ac55a0da8ee4f5
Василий Родин <varodg2000@mail.ru>
2022-05-09 17:25:50 +0300

    nothing

--- src/main/java/com/app/service/mail/kafka/KafkaProducer.java
+++ src/main/java/com/app/service/mail/kafka/KafkaProducer.java
@@ -1,18 +1,31 @@
 package com.app.service.mail.kafka;
 
-import com.app.controller.ActorController;
 import com.app.model.emailInfo.NewEmail;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.time.Duration;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 @Service
+@EnableScheduling
 public class KafkaProducer {
+
+    private static final Duration d = java.time.Duration.parse("PT1M");
+
+    private ConcurrentLinkedQueue<NewEmail> concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
+
+    public void addEmailToQueue(NewEmail newEmail) {
+        concurrentLinkedQueue.add(newEmail);
+    }
 
     private static final Logger logger = Logger.getLogger(KafkaProducer.class);
     @Value(value = "${spring.kafka.topic-name}")
@@ -21,22 +34,29 @@ public class KafkaProducer {
     @Autowired
     private KafkaTemplate<String, NewEmail> kafkaTemplate;
 
-    public void sendMessage(NewEmail email) {
+    @Scheduled(initialDelay = 60000, fixedRate = 60000)
+    public void sendMessage() {
 
-        ListenableFuture<SendResult<String, NewEmail>> future = kafkaTemplate.send(topicName, email);
+        if (concurrentLinkedQueue.size() != 0) {
+            NewEmail email = concurrentLinkedQueue.remove();
 
-        future.addCallback(new ListenableFutureCallback<SendResult<String, NewEmail>>() {
+            ListenableFuture<SendResult<String, NewEmail>> future = kafkaTemplate.send(topicName, email);
 
-            @Override
-            public void onSuccess(SendResult<String, NewEmail> result) {
-                logger.info("Sent message=[" + email + "] with offset=[" + result.getRecordMetadata().offset() + "]");
-            }
+            future.addCallback(new ListenableFutureCallback<SendResult<String, NewEmail>>() {
 
-            @Override
-            public void onFailure(Throwable ex) {
-                logger.warn("Unable to send message=[" + email + "] due to : " + ex.getMessage());
-            }
-        });
+                @Override
+                public void onSuccess(SendResult<String, NewEmail> result) {
+                    logger.info(
+                            "Sent message=[" + email + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                }
+
+                @Override
+                public void onFailure(Throwable ex) {
+                    logger.warn("Unable to send message=[" + email + "] due to : " + ex.getMessage());
+                }
+            });
+        }
+
     }
 
 }
